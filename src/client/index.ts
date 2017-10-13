2eac16ddbab876207cdf35cd279582c472e9982d
leon-andria <andrianarivony@gmail.com>
2017-10-14 00:14:00 +0200

    fff

---
+++ src/client/index.ts
@@ -0,0 +1,14 @@
+async function automaticallyLoadSplitBundle() {
+    // this file is automatically associated with the bundle "about"
+    // you could do import("about") and get the same result, but typings would go wrong
+    const aboutModule = await import("about");
+    console.log(new aboutModule.AboutComponent());
+
+    const homeComponent = await import("./routes/home/HomeComponent");
+    console.log(new homeComponent.HomeComponent());    
+}
+
+automaticallyLoadSplitBundle();
+
+
+
